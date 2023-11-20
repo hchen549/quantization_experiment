188794baa79afe27eadc9fb40775155f1486b8b0
hchen549 <hchen549@wisc.edu>
2023-11-20 04:40:18 +0000

    initial the env and code

---
+++ utils/model.py
@@ -0,0 +1,34 @@
+
+def get_model_flops(model, inputs):
+    num_macs = profile_macs(model, inputs)
+    return num_macs
+
+def get_model_size(model: nn.Module, data_width=32):
+    """
+    calculate the model size in bits
+    :param data_width: #bits per element
+    """
+    num_elements = 0
+    for param in model.parameters():
+        num_elements += param.numel()
+    return num_elements * data_width
+
+def download_url(url, model_dir='.', overwrite=False):
+    import os, sys
+    from urllib.request import urlretrieve
+    target_dir = url.split('/')[-1]
+    model_dir = os.path.expanduser(model_dir)
+    try:
+        if not os.path.exists(model_dir):
+            os.makedirs(model_dir)
+        model_dir = os.path.join(model_dir, target_dir)
+        cached_file = model_dir
+        if not os.path.exists(cached_file) or overwrite:
+            sys.stderr.write('Downloading: "{}" to {}\n'.format(url, cached_file))
+            urlretrieve(url, cached_file)
+        return cached_file
+    except Exception as e:
+        # remove lock file so download can be executed next time.
+        os.remove(os.path.join(model_dir, 'download.lock'))
+        sys.stderr.write('Failed to download from url %s' % url + '\n' + str(e) + '\n')
+        return None
